8131f811a7c3f8e130a54c39464333ca2fa1a926
EmilyJaynee <emily.barrott@hotmail.co.uk>
2025-04-06 19:33:21 +0100

    added basic JS and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem() {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Insert Poem here 🌹",
+    autoStart: true,
+    cursor: null,
+  });
+}
+
+let poemGeneraterForm = document.querySelector("#poem-generator-form");
+poemGeneraterForm.addEventListener("submit", generatePoem);
