ab55568b6d533e89a4295aa19583746311ade719
EmilyJaynee <emily.barrott@hotmail.co.uk>
2025-04-07 12:20:26 +0100

    Added AI api to my JS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+function poemApi() {
+  let apiKey = "19884f8731abea4oebtff3a019e58351";
+  let prompt = "Travel poem Generator";
+  let context =
+    "You are a travel poem generator. You write short, travel based poems about the subject provided";
+  let aiApi = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log(aiApi);
+}
+
 function generatePoem() {
   event.preventDefault();
 
@@ -10,3 +19,5 @@ function generatePoem() {
 
 let poemGeneraterForm = document.querySelector("#poem-generator-form");
 poemGeneraterForm.addEventListener("submit", generatePoem);
+
+poemApi();
