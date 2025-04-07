f7f71a2d84e658ea0595d0fe0fbf545950720f5a
EmilyJaynee <emily.barrott@hotmail.co.uk>
2025-04-07 13:07:14 +0100

    integrated api to generate a poem based on the user input

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-function poemApi() {
-  let apiKey = "19884f8731abea4oebtff3a019e58351";
-  let prompt = "Travel poem Generator";
-  let context =
-    "You are a travel poem generator. You write short, travel based poems about the subject provided";
-  let aiApi = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log(aiApi);
+function typePoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 4,
+  });
 }
 
 function generatePoem() {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "Insert Poem here 🌹",
-    autoStart: true,
-    cursor: null,
-  });
+  let poemPrompt = document.querySelector("#poem-prompt");
+  let apiKey = "19884f8731abea4oebtff3a019e58351";
+  let context =
+    "You are a travel poem generator. You love to write a short, 4 lined poem. Make sure to follow user instructions";
+  let prompt = `user instructions: Generate a poem about travel using the theme ${poemPrompt.value}`;
+  let aiApi = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(`prompt. ${prompt}`);
+  console.log(context);
+
+  axios.get(aiApi).then(typePoem);
 }
 
 let poemGeneraterForm = document.querySelector("#poem-generator-form");
 poemGeneraterForm.addEventListener("submit", generatePoem);
-
-poemApi();
